31ee83f95f06676c9651c8bddd27e7c327d2caca
Gaël Écorchard <gael.ecorchard@cvut.cz>
2021-02-24 21:28:28 +0100

    [ranger] View images with kitty
    - View images with kitty.
    - Preview pdf as image.

--- private_dot_config/ranger/executable_scope.sh
+++ private_dot_config/ranger/executable_scope.sh
@@ -65,14 +65,14 @@ handle_extension() {
             exit 1;;
 
         ## PDF
-        pdf)
-            ## Preview as text conversion
-            pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | \
-              fmt -w "${PV_WIDTH}" && exit 5
-            mutool draw -F txt -i -- "${FILE_PATH}" 1-10 | \
-              fmt -w "${PV_WIDTH}" && exit 5
-            exiftool "${FILE_PATH}" && exit 5
-            exit 1;;
+        # pdf)
+        #     ## Preview as text conversion
+        #     pdftotext -l 10 -nopgbrk -q -- "${FILE_PATH}" - | \
+        #       fmt -w "${PV_WIDTH}" && exit 5
+        #     mutool draw -F txt -i -- "${FILE_PATH}" 1-10 | \
+        #       fmt -w "${PV_WIDTH}" && exit 5
+        #     exiftool "${FILE_PATH}" && exit 5
+        #     exit 1;;
 
         ## BitTorrent
         torrent)
@@ -163,14 +163,14 @@ handle_image() {
         #     exit 1;;
 
         ## PDF
-        # application/pdf)
-        #     pdftoppm -f 1 -l 1 \
-        #              -scale-to-x "${DEFAULT_SIZE%x*}" \
-        #              -scale-to-y -1 \
-        #              -singlefile \
-        #              -jpeg -tiffcompression jpeg \
-        #              -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
-        #         && exit 6 || exit 1;;
+        application/pdf)
+            pdftoppm -f 1 -l 1 \
+                     -scale-to-x "${DEFAULT_SIZE%x*}" \
+                     -scale-to-y -1 \
+                     -singlefile \
+                     -jpeg -tiffcompression jpeg \
+                     -- "${FILE_PATH}" "${IMAGE_CACHE_PATH%.*}" \
+                && exit 6 || exit 1;;
 
 
         ## ePub, MOBI, FB2 (using Calibre)
